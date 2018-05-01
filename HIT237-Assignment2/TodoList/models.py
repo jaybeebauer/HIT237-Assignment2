a768857dcbf48896f27a1da4ed8cfd4a0af429f0
Joshua Bauer <joshybee@gmail.com>
2018-05-01 22:13:25 +0800

    -Bug with UUID, resolved by resorting to standard ID for models

--- HIT237-Assignment2/TodoList/models.py
+++ HIT237-Assignment2/TodoList/models.py
@@ -3,9 +3,8 @@ import uuid
 
 # Create your models here.
 class Item(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     title = models.CharField(max_length=100, help_text='The summary of a todo item')
-    priority = models.ForeignKey(Priority)
+    priorityid = models.ForeignKey(Priority)
     tags = models.ManyToManyField(Tag)
     complete = models.BooleanField(help_text='Whether a todo item is complete or not')
     created = models.DateTimeField(auto_now_add=True, help_text='Date the todo item was created')
@@ -17,21 +16,18 @@ class Item(models.Model):
         return str('%s Due:%s' % (self.title, dueDate))
 
 class Priority(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=20, help_text='Unqiue identifier for each todo list item')
 
     def __str__(self):
         return self.name
 
 class Tag(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=50, help_text='Name of the tag item')
 
     def __str__(self):
         return self.name
 
 class Assignee(models.Model):
-    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     firstname = models.CharField(max_length=100, help_text='The first name of the assignee')
     lastname = models.CharField(max_length=100, help_text='The last name of the assignee')
     email = models.EmailField(help_text='Email of the assignee')
