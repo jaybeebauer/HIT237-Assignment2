ad8b015ee7645e6dbbcf77f8c0c7057495d84de0
Joshua Bauer <joshybee@gmail.com>
2018-05-03 06:57:46 +0800

    -Finalised base HTML and page links (views)

--- HIT237-Assignment2/HIT237_Assignment2/urls.py
+++ HIT237-Assignment2/HIT237_Assignment2/urls.py
@@ -13,4 +13,9 @@ from TodoList import views
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', views.home, name='home'),
+    url(r'^todolist/?$', views.todolist, name='todolist'),
+    url(r'^todolist/item/create/?$', views.adjustrecord, name='create'),
+    url(r'^todolist/item/update/?$', views.adjustrecord, name='update'),
+    url(r'^todolist/item/detail/?$', views.adjustrecord, name='detail'),
+    url(r'^todolist/item/delete/?$', views.adjustrecord, name='delete'),
 ]
